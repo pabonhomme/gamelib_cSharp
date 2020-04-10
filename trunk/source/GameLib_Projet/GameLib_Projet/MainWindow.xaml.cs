54a202c4ce34a25adbfedcacc35fb5fd55d60254
pabonhomme1 <pabonhomme1@b5dbcade-d453-49ef-93d5-17021cce6112>
2020-04-10 15:14:52 +0000

    ajout user control jeu liste et suppression trucs inutiles

--- trunk/source/GameLib_Projet/GameLib_Projet/MainWindow.xaml.cs
+++ trunk/source/GameLib_Projet/GameLib_Projet/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PageAccueil
+namespace GameLib_Projet
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
