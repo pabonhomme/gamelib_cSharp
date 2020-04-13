8e46c4ec0a718569bc77126a1a25fa8574be3b58
nochevassu <nochevassu@b5dbcade-d453-49ef-93d5-17021cce6112>
2020-04-13 22:17:10 +0000

    Modification UC_JeuVideo

---
+++ source/GameLib_Projet/GameLib_Projet/UC_JeuVideo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameLib_Projet
+{
+    /// <summary>
+    /// Logique d'interaction pour UC_JeuVideo.xaml
+    /// </summary>
+    public partial class UC_JeuVideo : UserControl
+    {
+        public UC_JeuVideo()
+        {
+            InitializeComponent();
+        }
+    }
+}
