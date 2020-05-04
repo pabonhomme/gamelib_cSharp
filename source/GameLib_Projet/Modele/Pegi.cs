f66cc28d6d0bb919db9f0cc351783a2747fdf2d3
pabonhomme1 <pabonhomme1@b5dbcade-d453-49ef-93d5-17021cce6112>
2020-05-04 20:44:54 +0000

    Ajout du projet modele et modif UC_CreationCompte.xaml

---
+++ source/GameLib_Projet/Modele/Pegi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modele
+{
+    public enum Pegi
+    {
+        zero,
+        trois = 3,
+        sept = 7,
+        douze = 12,
+        seize = 16,
+        dixHuits = 18,
+    }
+}
---
+++ source/GameLib_Projet/Modele/Utilisateur.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modele
+{
+    class Utilisateur
+    {
+        /// <summary>
+        /// Nom de l'utilisateur
+        /// </summary>
+        public string Nom { get; private set; }
+
+        /// <summary>
+        /// Prénom de l'utilisateur
+        /// </summary>
+        public string Prénom { get; private set; }
+
+        /// <summary>
+        /// Age de l'utilisateur
+        /// </summary>
+        public int Age { get; private set; }
+
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        /// <param name="nom">Nom de l'utilisateur</param>
+        /// <param name="prénom">Prénom de l'utilisateur</param>
+        /// <param name="age">Age de l'utilisateur</param>
+        public Utilisateur(string nom, string prénom, int age)
+        {
+            Nom = nom;
+            Prénom = prénom;
+            Age = age;
+        }
+    }
+}
