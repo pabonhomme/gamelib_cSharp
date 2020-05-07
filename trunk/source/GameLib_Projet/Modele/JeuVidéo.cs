b21c984344b00f0a7960c5dddd0cd6c395b7b15f
pabonhomme1 <pabonhomme1@b5dbcade-d453-49ef-93d5-17021cce6112>
2020-05-07 07:10:38 +0000

    modofication classes

--- trunk/source/GameLib_Projet/Modele/JeuVidéo.cs
+++ trunk/source/GameLib_Projet/Modele/JeuVidéo.cs
@@ -4,8 +4,37 @@ using System.Text;
 
 namespace Modele
 {
-    class JeuVidéo
+    public class JeuVidéo
     {
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        /// <param name="nom">Nom du jeu</param>
+        /// <param name="note">Note du jeu</param>
+        /// <param name="prix">Prix du jeu</param>
+        /// <param name="description">Description du jeu</param>
+        /// <param name="lienTrailer">Lien du trailer du jeu</param>
+        /// <param name="lienImage">Lien de l'image du jeu</param>
+        /// <param name="modeleEco">Modèle économique du jeu</param>
+        /// <param name="studioDev">Nom du studio de développement</param> 
+        /// <param name="genre">Genre du jeu</param>
+        /// <param name="pegi">Age minimum pour avoir le jeu du jeu</param>
+        /// <param name="plateForme">Plate-forme où le jeu est disponible</param>
+        public JeuVidéo(string nom, int note, float prix, string description, string lienTrailer, string lienImage, string modeleEco, string studioDev, Genre genre, Pegi pegi, PlateForme plateForme)
+        {
+            Nom = nom;
+            Note = note;
+            Prix = prix;
+            Description = description;
+            LienTrailer = lienTrailer;
+            LienImage = lienImage;
+            ModeleEco = modeleEco;
+            StudioDev = studioDev;
+            Genre = genre;
+            Pegi = pegi;
+            PlateForme = plateForme;
+        }
+
         /// <summary>
         /// Nom du jeu
         /// </summary>
@@ -25,11 +54,6 @@ namespace Modele
         /// Description du jeu
         /// </summary>
         public string Description { get; private set; }
-
-        /// <summary>
-        /// Surnom du jeu
-        /// </summary>
-        public string Surnom { get;  set; }
 
         /// <summary>
         /// Lien du trailer du jeu
@@ -52,28 +76,27 @@ namespace Modele
         public string StudioDev { get; private set; }
 
         /// <summary>
-        /// Constructeur
+        /// Genre du jeu
         /// </summary>
-        /// <param name="nom">Nom du jeu</param>
-        /// <param name="note">Note du jeu</param>
-        /// <param name="prix">Prix du jeu</param>
-        /// <param name="description">Description du jeu</param>
-        /// <param name="lienTrailer">Lien du trailer du jeu</param>
-        /// <param name="lienImage">Lien de l'image du jeu</param>
-        /// <param name="modeleEco">Modèle économique du jeu</param>
-        /// <param name="studioDev">Nom du studio de développement</param>
-        public JeuVidéo(String nom, int note, float prix, String description, String lienTrailer, String lienImage, String modeleEco, string studioDev )
-        {
-            Nom = nom;
-            Prix = prix;
-            Note = note;
-            Description = description;
-            LienTrailer = lienTrailer;
-            LienImage = lienImage;
-            ModeleEco = modeleEco;
-            StudioDev = studioDev;
-            Surnom = string.Empty;
-        }
+        public Genre Genre { get; private set; }
+
+        /// <summary>
+        /// Age minimum pour avoir le jeu du jeu
+        /// </summary>
+        public Pegi Pegi { get; private set; }
+
+        /// <summary>
+        /// Plate-forme où le jeu est disponible
+        /// </summary>
+        public PlateForme PlateForme { get; private set; }
+
+        /// <summary>
+        ///Informe si le jeu est favori ou non
+        /// </summary>
+        public bool EstFavori { get; set; } = false;
+
+
+
 
 
 
