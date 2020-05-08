ad63eb82c375c766bb1c6589d362e7fa4208c371
pabonhomme1 <pabonhomme1@b5dbcade-d453-49ef-93d5-17021cce6112>
2020-05-08 18:03:12 +0000

    Ajout des classes Administrateur.cs, Application.cs, et des tests

---
+++ trunk/source/GameLib_Projet/Test_JeuVidéo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Modele;
+
+namespace Test_JeuVidéo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test de la classe JeuVidéo");
+
+            JeuVidéo jeuvidéo1 = new JeuVidéo("Minecraft", 4, 29.9f, "Je suis une patate verte comme Minecraft", "oui.youtube.com", "img.bin", "Achat définitif", "Marloc", Genre.Aventure , Pegi.trois, PlateForme.Pc  );
+
+            Console.WriteLine(jeuvidéo1);
+        }
+    }
+}
