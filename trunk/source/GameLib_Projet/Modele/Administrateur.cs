7640170f7b4d2cba9eedba6f8b6b301cd7f4a819
pabonhomme1 <pabonhomme1@b5dbcade-d453-49ef-93d5-17021cce6112>
2020-05-10 17:51:01 +0000

    Ajout de la classe admin et Application

---
+++ trunk/source/GameLib_Projet/Modele/Administrateur.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modele
+{
+    public class Administrateur : UtilisateurConnecté
+    {
+        /// <summary>
+        /// Constructeur de l'administrateur
+        /// </summary>
+        /// <param name="nom">Nom de l'admin</param>
+        /// <param name="prénom">Prénom de l'admin</param>
+        /// <param name="dateNaissance">Date de naissance de l'admin</param>
+        /// <param name="pseudo">Pseudo de l'admin</param>
+        /// <param name="motDePasse">Mot de passe de l'admin</param>
+        /// <param name="mail">Mail de l'admin</param>
+        public Administrateur(string nom, string prénom, DateTime dateNaissance, string pseudo, string motDePasse, string mail) 
+            : base(nom, prénom, dateNaissance, pseudo, motDePasse, mail)
+        {
+        }
+    }
+}
