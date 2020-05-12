d825a6f3941a8b0b04e83ad0ab1debb20eb10d93
pabonhomme1 <pabonhomme1@b5dbcade-d453-49ef-93d5-17021cce6112>
2020-05-12 13:47:58 +0000

    Changement de application en manager + diff modifs

--- trunk/source/GameLib_Projet/Test_Manager/Program.cs
+++ trunk/source/GameLib_Projet/Test_Manager/Program.cs
@@ -2,7 +2,7 @@
 using Modele;
 using System.Collections.Generic;
 
-namespace Test_Application
+namespace Test_Manager
 {
     class Program
     {
@@ -10,7 +10,7 @@ namespace Test_Application
         {
             Console.WriteLine("Test de la classe Application\n\n");
 
-            Application appli = new Application();
+            Manager mngr = new Manager();
 
             JeuVidéo jeuvidéo1 = new JeuVidéo("Minecraft", 4, 29.9f, "Je suis Minecraft", "Minecraft.youtube.com", "imgMinecraft.bin", "Achat définitif", "Mojeig", Genre.Aventure, Pegi.Trois, new List<PlateForme>(){
             PlateForme.Pc,PlateForme.Ps4,PlateForme.Xbox360,PlateForme.XboxOne});
@@ -19,22 +19,21 @@ namespace Test_Application
             PlateForme.Pc,PlateForme.Ps4,PlateForme.Xbox360,PlateForme.XboxOne, PlateForme.Ps3 });
 
             Console.WriteLine("Création des utilisateurs\n\n");
-            appli.CréerUtilisateur(new Administrateur("Bonhomme", "Paul", new DateTime(2001, 11, 18), "paul_b63", "MotDePassePaul", "polo.clash@gmail.com"));
-            appli.CréerUtilisateur(new UtilisateurConnecté("Chevassus", "Noe", new DateTime(2001, 06, 21), "shotlouf", "MotDePasseNoe", "noe@orange.fr"));
+            mngr.CréerUtilisateur(new Administrateur("Bonhomme", "Paul", new DateTime(2001, 11, 18), "paul_b63", "MotDePassePaul", "polo.clash@gmail.com"));
+            mngr.CréerUtilisateur(new UtilisateurConnecté("Chevassus", "Noe", new DateTime(2001, 06, 21), "shotlouf", "MotDePasseNoe", "noe@orange.fr"));
 
             Console.WriteLine("Pour vous connecter, veuillez renseigner votre mot de passe");
-            Console.WriteLine(appli.Connexion(appli.RechercherUtilisateur("shotlouf"), Console.ReadLine()));
+            Console.WriteLine(mngr.Connexion(mngr.RechercherUtilisateur("paul_b63"), Console.ReadLine()));
 
             Console.WriteLine("\n\nAjout des jeux à l'application\n\n");
-            appli.AjouterJeu(jeuvidéo1, appli.UtilisateurCourant );
-            appli.AjouterJeu(jeuvidéo2, appli.UtilisateurCourant );
+            mngr.AjouterJeu(jeuvidéo1, mngr.UtilisateurCourant);
+            mngr.AjouterJeu(jeuvidéo2, mngr.UtilisateurCourant);
 
             Console.WriteLine("Ajout d'un favori par l'utilisateur courant\n\n");
-            appli.UtilisateurCourant.AjouterFav(jeuvidéo2);
+            mngr.UtilisateurCourant.AjouterFav(jeuvidéo2);
 
             Console.WriteLine("Description de l'application\n\n");
-            Console.WriteLine(appli);
-
+            Console.WriteLine(mngr);
         }
     }
 }
