ed18783b035d8bd199a58795125f94d5f9f05253
pabonhomme1 <pabonhomme1@b5dbcade-d453-49ef-93d5-17021cce6112>
2020-06-13 14:39:17 +0000

    oublie -> ajout enum modele ecp

---
+++ trunk/source/GameLib_Projet/Modele/ModeleEco.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modele
+{
+    public enum ModeleEco
+    {
+        Inconnu,
+        Mensuel,
+        Définitif,
+        Gratuit,
+    }
+}
