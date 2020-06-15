730bca736b398988cec7de96e35fee7d04dfac7b
pabonhomme1 <pabonhomme1@b5dbcade-d453-49ef-93d5-17021cce6112>
2020-06-15 02:27:35 +0000

    Modification des test et ajout des diagrammes sous forme jpeg car les pdf ne marchaient pas. C'est une fin de projet.

--- source/GameLib_Projet/Test_DataContract/Program.cs
+++ source/GameLib_Projet/Test_DataContract/Program.cs
@@ -9,18 +9,33 @@ namespace Test_DataContract
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test DataContract");
+            Console.WriteLine("Test DataContract\n\n");
 
+            Console.WriteLine("Instanciation du premier manager\n\n");
             Manager manager = new Manager(new StubDataManager());
-            manager.DataManager = new BddDataManager();
-           
 
-            //manager.ListeUtilisateur[1].AjouterFav(manager.ListeJeux[2]);
+            Console.WriteLine("changement de méthode de persistance\n\n");
+
+            manager.DataManager = new BddDataManager();
+
+            Console.WriteLine("Ajout d'un jeu en favori par un utilisateur\n\n");
+
+            manager.ListeUtilisateur[1].AjouterFav(manager.ListeJeux[2]);
+
+            Console.WriteLine("Affichage de l'utilisateur\n\n");
             Console.WriteLine(manager.ListeUtilisateur[0]);
+
+            Console.WriteLine("Sauvegarde des données\n\n");
             manager.SauvegardeDonnées();
 
+            Console.WriteLine("Affichage du premier managerr\n\n");
+            Console.WriteLine(manager);
+
+            Console.WriteLine("Instanciation d'un second manager\n\n");
             Manager manager2 = new Manager(new BddDataManager());
-            
+
+            Console.WriteLine("Affichage du second manager pour vérification persistance\n\n");
+            Console.WriteLine(manager2);
         }
     }
 }
